efd100435694049b67d1cc1c056732cc02425622
Donia-212 <d2o0n0i3a@gmail.com>
2026-01-11 14:44:43 +0400

    Table_12_Evaluation_Comparison_AlternateUS_M.R

--- Table_12_Evaluation_Comparison_AlternateUS_M.R
+++ Table_12_Evaluation_Comparison_AlternateUS_M.R
@@ -92,7 +92,7 @@ model_evaluate_3 <- rbind(model_evaluate_3, evaluate(test_3, bsts_3_pred$mean, m
 predict_3 <- predict_3 %>% mutate('BSTS' = bsts_3_pred$mean)
 
 
-setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic /US Primary')
+setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic/US Alternate')
 
 
 # NBEATS
@@ -164,7 +164,7 @@ model_evaluate_6 <- rbind(model_evaluate_6, evaluate(test_6, bsts_6_pred$mean, m
 predict_6 <- predict_6 %>% mutate('BSTS' = bsts_6_pred$mean)
 
 
-setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic /US Primary')
+setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic/US Alternate')
 
 
 # NBEATS
@@ -237,7 +237,7 @@ model_evaluate_12
 predict_12 <- predict_12 %>% mutate('BSTS' = bsts_12_pred$mean)
 
 
-setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic /US Primary')
+setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic/US Alternate')
 
 
 # NBEATS
@@ -311,7 +311,7 @@ model_evaluate_24 <- rbind(model_evaluate_24, evaluate(test_24, bsts_24_pred$mea
 predict_24 <- predict_24 %>% mutate('BSTS' = bsts_24_pred$mean)
 
 
-setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic /US Primary')
+setwd('Climate_Policy_Uncertainty_Forecasting/Dataset/Dataset_Deep_Learning_Models_Forecasts/Models - Macroeconomic/US Alternate')
 
 
 # NBEATS
